d2de5683c119c9dffbfc9d278ce9343562594cb8
Camden Rudisill <camden.rudisill@richmond.edu>
2016-03-10 12:46:56 -0800

    Add: more comments to code

--- src/components/color-output.jsx
+++ src/components/color-output.jsx
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 
+
 export default(props) => {
+  // set div's background color via its props
+  // and add bottom shadow to planet to make it appear floating
   return (
-    // set div's background color via its props
     <div className="color-output" style={{
       backgroundColor: props.color
     }}>
-      // Add bottom shadow to planet to make it appear floating
+
       <div className="shadow"></div>
     </div>
   );
